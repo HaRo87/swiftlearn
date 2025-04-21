4590b649142243e0776dbb1e1657d0c09c7085be
HaRo87 <github@fam-hansel.de>
2025-04-21 09:02:33 +0000

    initial setup

---
+++ Sources/main.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+let initValue: Int = 500
+var addValue: Int = 50
+var finalValue: Int = initValue + addValue
+
+print("\(initValue) + \(addValue) = \(finalValue)")
